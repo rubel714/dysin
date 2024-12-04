bf0b1dad4ff3ec37dbcc8f79bb87fd59737d1f2c
rubel714 <rubel714@gmail.com>
2024-12-04 23:11:55 +0600

    database backup

--- src/components/Footers/DarkFooter.js
+++ src/components/Footers/DarkFooter.js
@@ -28,7 +28,7 @@ function DarkFooter() {
         </div> */}
 
         <div class="footer">
-            <h6>Powred By NextGen Software & Solutions Ltd. &#169; Reserved (v0.2)</h6>
+            <h6>Powred By NextGen Software & Solutions Ltd. &#169; Reserved (v0.3)</h6>
         </div>
       </Container>
     </footer>
